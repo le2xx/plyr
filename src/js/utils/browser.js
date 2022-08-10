11456300cf71d932df75394be67630d8d81f3f2f
le2xx <le2x@rambler.ru>
2022-08-10 19:41:37 +0300

    check is window

--- src/js/utils/browser.js
+++ src/js/utils/browser.js
@@ -2,15 +2,17 @@
 // Browser sniffing
 // Unfortunately, due to mixed support, UA sniffing is required
 // ==========================================================================
+const isWindow = typeof window === 'undefined';
 
 const browser = {
-  isIE: Boolean(window.document.documentMode),
-  isEdge: window.navigator.userAgent.includes('Edge'),
-  isWebkit: 'WebkitAppearance' in document.documentElement.style && !/Edge/.test(navigator.userAgent),
-  isIPhone: /(iPhone|iPod)/gi.test(navigator.platform),
-  isIos:
-    (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1) ||
-    /(iPad|iPhone|iPod)/gi.test(navigator.platform),
+  isIE: isWindow ? Boolean(window.document.documentMode) : false,
+  isEdge: isWindow ? window.navigator.userAgent.includes('Edge') : false,
+  isWebkit: 'WebkitAppearance' in window.document.documentElement.style && !/Edge/.test(window.navigator.userAgent),
+  isIPhone: isWindow ? /(iPhone|iPod)/gi.test(window.navigator.platform) : false,
+  isIos: isWindow
+    ? (window.navigator.platform === 'MacIntel' && window.navigator.maxTouchPoints > 1) ||
+      /(iPad|iPhone|iPod)/gi.test(window.navigator.platform)
+    : false,
 };
 
 export default browser;
